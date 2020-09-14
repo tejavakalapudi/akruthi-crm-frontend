18a721654784d389cafbe5aa4ab59479ad105935
Raviteja Vakalapudi <tejavakalapudi@gmail.com>
2020-09-14 07:26:42 +0530

    Popover fix

--- app/containers/Leads.js
+++ app/containers/Leads.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/no-array-index-key */
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,6 +11,7 @@ import TableRow from '@material-ui/core/TableRow';
 import Checkbox from '@material-ui/core/Checkbox';
 import Paper from '@material-ui/core/Paper';
 import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
 import { LeadsActions } from '../redux/actions';
 import { ReactHelmet } from '../components';
 import PopOver from '../components/PopOver';
@@ -69,76 +71,95 @@ function Leads() {
     setAnchorEl(null);
   };
 
-  const open = Boolean(anchorEl);
+  const handleBulkUpload = (e) => {
+    if (!e.target.files.length) {
+      console.log('no files');
+    } else {
+      dispatch(LeadsActions.bulkUploadLeads(e.target.files[0]));
+    }
+  };
 
   return (
-    <Paper className={classes.paper}>
+    <div>
       <ReactHelmet title="Dashboard" meta="Discover your leads" />
-
-      <Table className={classes.table} size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell component="th" className={classes.headerCell}>
-              <Checkbox
-                color="primary"
-                size="small"
-                onClick={handleToggle}
-                checked={checkedItems.length === itemIds.length}
-              />
-            </TableCell>
-            <TableCell color="primary" className={classes.headerCell}>
-              Name
-            </TableCell>
-            <TableCell className={classes.headerCell}>Contact</TableCell>
-            <TableCell className={classes.headerCell}>Venture</TableCell>
-            <TableCell className={classes.headerCell}>Status</TableCell>
-            <TableCell className={classes.headerCell}>Employee Assigned</TableCell>
-            <TableCell className={classes.headerCell}>FollowUp Required</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {userData.map((row, index) => (
-            <TableRow key={index}>
-              <TableCell component="th" scope="row">
+      <div style={{ display: 'flex', justifyContent: 'flex-end', margin: '10px 0' }}>
+        <input
+          style={{ display: 'none' }}
+          id="contained-button-file"
+          multiple
+          type="file"
+          onChange={handleBulkUpload}
+        />
+        <label htmlFor="contained-button-file">
+          <Button variant="contained" color="primary" component="span">
+            Bulk Upload
+          </Button>
+        </label>
+      </div>
+      <Paper className={classes.paper}>
+        <Table className={classes.table} size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell component="th" className={classes.headerCell}>
                 <Checkbox
-                  key={row.id}
-                  checked={checkedItems.includes(row.id)}
                   color="primary"
                   size="small"
-                  onClick={() => {
-                    handleRowCheck(row.id);
-                  }}
+                  onClick={handleToggle}
+                  checked={checkedItems.length === itemIds.length}
                 />
               </TableCell>
-              <TableCell component="th">{row.customer_name}</TableCell>
-              <TableCell>{row.contact}</TableCell>
-              <TableCell>{`${row.venture.name}, ${row.flat_No}`}</TableCell>
-              <TableCell>
-                <Chip
-                  label={formatStatusField(row.status)}
-                  classes={{ root: 'chip' }}
-                  className={row.status}
-                  onClick={handleClick}
-                  SelectProps={{
-                    native: true,
-                  }}
-                />
-
-                <PopOver
-                  formatStatusField={formatStatusField}
-                  statusList={statusList}
-                  open={open}
-                  anchorEl={anchorEl}
-                  onClose={handleClose}
-                />
+              <TableCell color="primary" className={classes.headerCell}>
+                Name
               </TableCell>
-              <TableCell>{row.employee_assigned.name}</TableCell>
-              <TableCell>{`${row.followup_required}`}</TableCell>
+              <TableCell className={classes.headerCell}>Contact</TableCell>
+              <TableCell className={classes.headerCell}>Venture</TableCell>
+              <TableCell className={classes.headerCell}>Status</TableCell>
+              <TableCell className={classes.headerCell}>Employee Assigned</TableCell>
+              <TableCell className={classes.headerCell}>FollowUp Required</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
+          </TableHead>
+          <TableBody>
+            {userData.map((row, index) => (
+              <TableRow key={index}>
+                <TableCell component="th" scope="row">
+                  <Checkbox
+                    key={row.id}
+                    checked={checkedItems.includes(row.id)}
+                    color="primary"
+                    size="small"
+                    onClick={() => {
+                      handleRowCheck(row.id);
+                    }}
+                  />
+                </TableCell>
+                <TableCell component="th">{row.customer_name}</TableCell>
+                <TableCell>{row.contact}</TableCell>
+                <TableCell>{`${row.venture.name}, ${row.flat_No}`}</TableCell>
+                <TableCell>
+                  <Chip
+                    label={formatStatusField(row.status)}
+                    classes={{ root: 'chip' }}
+                    className={row.status}
+                    onClick={handleClick}
+                    SelectProps={{
+                      native: true,
+                    }}
+                  />
+                </TableCell>
+                <TableCell>{row.employee_assigned.name}</TableCell>
+                <TableCell>{`${row.followup_required}`}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <PopOver
+            formatStatusField={formatStatusField}
+            statusList={statusList}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+          />
+        </Table>
+      </Paper>
+    </div>
   );
 }
 export default Leads;
